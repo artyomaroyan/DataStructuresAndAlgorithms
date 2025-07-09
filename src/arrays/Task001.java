053a4fa44631268e8d39997351bb3e424ac69028
Artyom <aroyanart98@gmail.com>
2025-07-09 21:40:56 +0400

    Add initial Task001.

---
+++ src/arrays/Task001.java
@@ -0,0 +1,12 @@
+package arrays;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 09.07.25
+ * Time: 16:43:53
+ * <p>
+ * Find the maximum element in an array.
+ * Traverse the array and compare each element with a variable max. Update max if a bigger element is found.
+ */
+public class Task001 {
+}
