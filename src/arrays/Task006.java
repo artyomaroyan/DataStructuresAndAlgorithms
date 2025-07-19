266e076fab26660557bbb6d701d08fe0ec924c7f
Artyom <aroyanart98@gmail.com>
2025-07-19 21:44:29 +0400

    feat(task006):
    - create task006

---
+++ src/arrays/Task006.java
@@ -0,0 +1,21 @@
+package arrays;
+
+import java.util.logging.Logger;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 19.07.25
+ * Time: 01:09:47
+ * <p>
+ * Find the second largest element.
+ * Track both largest and second largest as you traverse.
+ * Hint: Don't just sort; think in O(n) without sorting.
+ */
+public class Task006 {
+    private static final Logger logger = Logger.getLogger(Task006.class.getName());
+    public static void main(String[] args) {
+
+    }
+
+
+}
