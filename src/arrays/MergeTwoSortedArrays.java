1da10eb6b1e97778ca9d7a325111b9f5e931ee0e
Artyom <aroyanart98@gmail.com>
2025-08-06 22:57:38 +0400

    feat:
    create `MergeTwoSortedArrays` task.

---
+++ src/arrays/MergeTwoSortedArrays.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import java.util.logging.Logger;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 05.08.25
+ * Time: 23:20:22
+ */
+public class MergeTwoSortedArrays {
+    private static final Logger logger = Logger.getLogger(MergeTwoSortedArrays.class.getName());
+    public static void main(String[] args) {
+    }
+
+}
