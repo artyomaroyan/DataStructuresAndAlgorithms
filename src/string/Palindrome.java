e69b52267278ee86c93f92c1df11e45f66611f9f
Artyom <aroyanart98@gmail.com>
2025-08-10 23:11:21 +0400

    refactor(Palindrome):
    - add two more methods to check if String is palindrome with ignoring middle char if exists.

--- src/string/Palindrome.java
+++ src/string/Palindrome.java
@@ -15,6 +15,8 @@ public class Palindrome {
         logger.log(Level.INFO, () -> "Result: " + isPalindrome(str));
     }
 
+    // this is fine but this will return false for "racecar" but should return ture,
+    // so I need to ignore middle char if lengt is odd number.
     private static boolean isPalindrome(String str) {
         String prefix = str.substring(0, str.length() / 2);
         String suffix = str.substring(str.length() / 2);
@@ -22,5 +24,22 @@ public class Palindrome {
         var reversed = sb.reverse().toString();
 
         return reversed.equals(suffix);
+    }
+
+    private static boolean isPalindromeOdd(String str) {
+        int length = str.length();
+        String prefix = str.substring(0, length / 2);
+        String suffix = str.substring((length + 1) / 2); // skips middle char if odd
+        return new StringBuilder(prefix).reverse().toString().equals(suffix);
+    }
+
+    private static boolean isPalindromeOdd2(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left++) != str.charAt(right--))
+                return false;
+        }
+        return true;
     }
 }
