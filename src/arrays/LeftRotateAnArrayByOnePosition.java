e2676617b99cf58a4711e8a012bb716a3495fcec
Artyom <aroyanart98@gmail.com>
2025-08-18 23:30:23 +0400

    feat:
    - create task `Left rotate an array by one position.`

---
+++ src/arrays/LeftRotateAnArrayByOnePosition.java
@@ -0,0 +1,12 @@
+package arrays;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 18.08.25
+ * Time: 23:28:30
+ * Task 010
+ * Explanation: Move all elements one step left, and put the first element at the last.
+ * Hint: Save the first element before shifting.
+ */
+public class LeftRotateAnArrayByOnePosition {
+}
