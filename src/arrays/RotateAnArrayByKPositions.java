ad1c096cdef3eadd6375af44a11b885978a6c054
Artyom <aroyanart98@gmail.com>
2025-08-18 23:30:43 +0400

    feat:
    - create task `Rotate an array by k positions.`

---
+++ src/arrays/RotateAnArrayByKPositions.java
@@ -0,0 +1,12 @@
+package arrays;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 18.08.25
+ * Time: 23:29:33
+ * Task 011
+ * Explanation: Rotate left/right by k. Do it efficiently, not by rotating one by one.
+ * Hint: Try reversing parts of the array.
+ */
+public class RotateAnArrayByKPositions {
+}
