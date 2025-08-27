289d6602724bb38b74d35fda44581e36937157a0
Artyom <aroyanart98@gmail.com>
2025-08-28 01:46:08 +0400

    feat: just add `FindTheNumber` class.

---
+++ src/arrays/FindTheNumber.java
@@ -0,0 +1,16 @@
+package arrays;
+
+import java.util.logging.Logger;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 28.08.25
+ * Time: 01:21:31
+ * Task 013
+ * Find the number that appears once when all others appear twice.
+ * Explanation: Use XOR. x^x = 0, and x^0 = x.
+ * Hint: XOR all elements together.
+ */
+public class FindTheNumber {
+
+}
