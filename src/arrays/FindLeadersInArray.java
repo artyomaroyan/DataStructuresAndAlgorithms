f29599aabfd1b80b74bb0e3dbcc054269e041772
Artyom <aroyanart98@gmail.com>
2025-09-02 21:40:45 +0400

    feat: add comment for `FindLeadersInArray` class.

--- src/arrays/FindLeadersInArray.java
+++ src/arrays/FindLeadersInArray.java
@@ -7,6 +7,9 @@ import java.util.logging.Logger;
  * Author: Artyom Aroyan
  * Date: 29.08.25
  * Time: 23:19:25
+ * Task016
+ * Explanation: Traverse from right to left, track maximum so far.
+ * Hint: Last element is always a leader.
  */
 public class FindLeadersInArray {
     private static final Logger logger = Logger.getLogger(FindLeadersInArray.class.getName());
