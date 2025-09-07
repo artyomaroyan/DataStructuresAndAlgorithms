ccce9f916ce9501d12bd9fb8948dbbc71aa24279
Artyom <aroyanart98@gmail.com>
2025-09-07 23:09:41 +0400

    feat: add different implementation of Singleton pattern.

--- src/designpatterns/Singleton.java
+++ src/designpatterns/Singleton.java
@@ -6,15 +6,72 @@ package designpatterns;
  * Time: 23:09:09
  */
 public final class Singleton {
-    private static Singleton instance;
-
     private Singleton() {
     }
 
-    public static Singleton getInstance() {
-        if (instance == null) {
-            instance = new Singleton();
+    // this is implementation of singleton lazy initialization
+    // this is not thread safe
+    private static class LazyInitializedSingleton {
+        private static LazyInitializedSingleton instance;
+
+        private LazyInitializedSingleton() {
         }
-        return instance;
+
+        public static LazyInitializedSingleton getInstance() {
+            if (instance == null) {
+                instance = new LazyInitializedSingleton();
+            }
+            return instance;
+        }
+    }
+
+    // this is implementation of thread safe singleton initialization.
+    // this is thread safe but slower.
+    private static class ThreadSafeSingleton {
+        private static ThreadSafeSingleton instance;
+
+        private ThreadSafeSingleton() {
+        }
+
+        public static synchronized ThreadSafeSingleton getInstance() {
+            if (instance == null) {
+                instance = new ThreadSafeSingleton();
+            }
+            return instance;
+        }
+    }
+
+    // this is double-checked locking singleton initialization.
+    // this is thread safe and faster than only thread safe version.
+    private static class DoubleCheckedLockingSingleton {
+        private static DoubleCheckedLockingSingleton instance;
+
+        private DoubleCheckedLockingSingleton() {
+        }
+
+        public static DoubleCheckedLockingSingleton getInstance() {
+            if (instance == null) {
+                synchronized (DoubleCheckedLockingSingleton.class) {
+                    if (instance == null) {
+                        instance = new DoubleCheckedLockingSingleton();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+    // this is best practice thread safe, lazy initialize and no synchronized overhead.
+    private static class BillPughSingleton {
+        private BillPughSingleton() {
+        }
+
+        private static class SingletonHelper {
+            private static final BillPughSingleton INSTANCE = new BillPughSingleton();
+        }
+
+        public static BillPughSingleton getInstance() {
+            return SingletonHelper.INSTANCE;
+        }
     }
 }
