c43ff0ccf3687f2281d615636c2ffc5cd26100ab
Artyom <aroyanart98@gmail.com>
2025-09-13 00:12:55 +0400

    style: add comments for `FindTheMaximumSumSubarray`

--- src/arrays/FindTheMaximumSumSubarray.java
+++ src/arrays/FindTheMaximumSumSubarray.java
@@ -4,6 +4,10 @@ package arrays;
  * Author: Artyom Aroyan
  * Date: 02.09.25
  * Time: 21:41:39
+ * Task016
+ * Find the maximum sum subarray (Kadane's Algorithm - easy version).
+ * Explanation: Maintain current sum and maximum sum.
+ * Hint: If current sum drops below 0, reset to 0.
  */
 public class FindTheMaximumSumSubarray {
 }
