ad7f2b55c62980f2f2668052f4970f021d16206d
Artyom <aroyanart98@gmail.com>
2025-09-13 00:12:42 +0400

    style: add comments for `FindTheMajorityElement`

--- src/arrays/FindTheMajorityElement.java
+++ src/arrays/FindTheMajorityElement.java
@@ -4,6 +4,10 @@ package arrays;
  * Author: Artyom Aroyan
  * Date: 02.09.25
  * Time: 21:42:54
+ * Task019
+ * Find the majority element (appears more than n/2 times).
+ * Explanation: Use Boyer-Moore voting algorithm.
+ * Hint: Find candidate first, then validate.
  */
 public class FindTheMajorityElement {
 }
