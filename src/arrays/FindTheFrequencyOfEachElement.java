5b495a080972cd46d9f8978151020aedc3158c99
Artyom <aroyanart98@gmail.com>
2025-09-17 14:50:17 +0400

    refactor: implement solution for `FindTheFrequencyOfEachElement` task.

--- src/arrays/FindTheFrequencyOfEachElement.java
+++ src/arrays/FindTheFrequencyOfEachElement.java
@@ -1,6 +1,7 @@
 package arrays;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 /**
@@ -16,15 +17,34 @@ public class FindTheFrequencyOfEachElement {
     private static final Logger log = Logger.getLogger(FindTheFrequencyOfEachElement.class.getName());
 
     public static void main(String[] args) {
-
+        test("Test_1", new int[]{-3, 3, -1, -1, 0, 1, -5, -4, -5, -2});
+        test("Test_2", new int[]{1, 1, 0, 1, 1, 0, 3, -4, 1, 4, 0});
+        test("Test_3", new int[]{-4, -2, 1, -2, 1, 4, 0, 1, 2, -3, -5, -5});
+        test("Test_4", new int[]{1, 3, -3, -2, 0, 3, 4, -3, -2, -4, -1});
     }
 
     private static void test(String label, int[] array) {
-        log.info(() -> label + " result: " + Arrays.toString(array));
+        Map<Integer, Integer> result = frequencyOfEachElement(array);
+        log.info(() -> label + " result: " + result);
     }
 
-    private static void findSumSubarray(int[] array) {
-        int currentSum = 0;
-        int maxSum = Integer.MIN_VALUE;
+    private static HashMap<Integer, Integer> frequencyOfEachElement(int[] array) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        // option 1
+        for (int num : array) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        return map;
+
+        // option 2
+//        for (int num : array) {
+//            if (map.containsKey(num)) {
+//                int oldCount = map.get(num);
+//                map.put(num, oldCount + 1);
+//            } else {
+//                map.put(num, 1);
+//            }
+//        }
+//        return map;
     }
 }
