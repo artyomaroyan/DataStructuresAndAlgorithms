7c5337966ec1af1cf4afc4e5ee833d56a52730a2
Artyom <aroyanart98@gmail.com>
2025-09-22 21:30:45 +0400

    refactor:  improve `ReverseString` class.

--- src/string/ReverseString.java
+++ src/string/ReverseString.java
@@ -10,13 +10,29 @@ import java.util.logging.Logger;
  * Author: Artyom Aroyan
  * Date: 04.08.25
  * Time: 18:56:41
+ * Task001
  */
 public class ReverseString {
     private static final Logger logger = Logger.getLogger(ReverseString.class.getName());
-    public static void main(String[] args) {
-        String str = "adb";
-        reverseViaCollections(str);
 
+    public static void main(String[] args) {
+        test("Test_1", "asdad");
+        test("Test_2", "reyrt");
+        test("Test_3", "sfsdkm");
+        test("Test_4", "fghjgh");
+    }
+
+    private static void test(String label, String str) {
+        String builderResult = reverseViaBuilder(str);
+        logger.info(() -> label + " reverse with builder: " + builderResult);
+        String bufferResult = reverseViaBuffer(str);
+        logger.info(() -> label + " reverse with buffer: " + bufferResult);
+        String charArrayResult = reverseViaCharArray(str);
+        logger.info(() -> label + " revers with char array: " + charArrayResult);
+        String byteResult = reverseViaBytes(str);
+        logger.info(() -> label + " revers with byte array: " + byteResult);
+        String collectionResult = reverseViaCollections(str);
+        logger.info(() -> label + " revers with collections: " + collectionResult);
     }
 
     private static String reverseViaBuilder(String str) {
@@ -30,10 +46,13 @@ public class ReverseString {
         return sb.reverse().toString();
     }
 
-    private static void reverseViaCharArray(String str) {
+    private static String reverseViaCharArray(String str) {
         char[] charArray = str.toCharArray();
-        for (int i = charArray.length - 1; i >= 0; i--)
-            System.out.println(charArray[i]);
+        StringBuilder sb = new StringBuilder();
+        for (int i = charArray.length - 1; i >= 0; i--) {
+            sb.append(charArray[i]);
+        }
+        return sb.toString();
     }
 
     private static String reverseViaBytes(String str) {
@@ -46,19 +65,20 @@ public class ReverseString {
         return new String(res);
     }
 
-    private static void reverseViaCollections(String str) {
+    private static String reverseViaCollections(String str) {
         char[] arr = str.toCharArray();
         List<Character> list = new ArrayList<>();
         for (char c : arr)
             list.add(c);
 
-        // this is also work
+        // this also works
 //        var listReversed = list.reversed();
 
         Collections.reverse(list);
 
-        ListIterator it  = list.listIterator();
+        ListIterator<Character> it = list.listIterator();
         while (it.hasNext())
             System.out.print(it.next());
+        return String.valueOf(list);
     }
 }
