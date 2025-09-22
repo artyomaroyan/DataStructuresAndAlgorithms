9d0a411bf3878d7756d2ad1dada7ef5e1d398534
Artyom <aroyanart98@gmail.com>
2025-09-22 21:31:28 +0400

    refactor: implement solution for `CountVowels` task.

---
+++ src/string/CountVowels.java
@@ -0,0 +1,39 @@
+package string;
+
+import java.util.Set;
+import java.util.logging.Logger;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 22.09.25
+ * Time: 21:15:05
+ * Task003
+ */
+public class CountVowels {
+    private static final Logger logger = Logger.getLogger(CountVowels.class.getName());
+    private static final Set<Character> VOWELS = Set.of('A', 'E', 'I', 'O', 'U');
+
+    public static void main(String[] args) {
+        test("Test_1", "hellOo");
+        test("Test_2", "world");
+        test("Test_3", "JAVA");
+        test("Test_4", "somEthIng");
+    }
+
+    private static void test(String label, String str) {
+        int result = countVowels(str);
+        logger.info(() -> label + " result: " + result);
+    }
+
+    private static int countVowels(String str) {
+        int count = 0;
+        String upper = str.toUpperCase();
+
+        for (char ch : upper.toCharArray()) {
+            if (VOWELS.contains(ch)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
