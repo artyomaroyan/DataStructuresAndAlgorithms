f797bbfa66b511637b996ce6bc7094828cb71de3
Artyom <aroyanart98@gmail.com>
2025-09-23 16:31:02 +0400

    feat: TODO: complete `FamilyTree` task.

---
+++ src/list/FamilyTree.java
@@ -0,0 +1,17 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 20.09.25
+ * Time: 21:48:39
+ */
+public class FamilyTree {
+    public static void main(String[] args) {
+        String name;
+        FamilyTree parent;
+        List<FamilyTree> list = new ArrayList<>();
+    }
+}
