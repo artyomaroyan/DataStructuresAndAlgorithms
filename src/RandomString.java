78d5d37f987fd1a524b3c173a1fc398da1b35aa8
Artyom <aroyanart98@gmail.com>
2025-09-25 18:18:19 +0400

    feat: initial commit for `RandomString` class.

---
+++ src/RandomString.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 25.09.25
+ * Time: 16:02:15
+ */
+public class RandomString {
+    private static final Random random = new Random();
+    private static final Logger log = Logger.getLogger(RandomString.class.getName());
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static void main(String[] args) {
+        String result = generateRandomString();
+        log.info(() -> "Random String " + result);
+    }
+
+    private static String generateRandomString() {
+        return random
+                .ints(15, 0, CHARACTERS.length())
+                .mapToObj(CHARACTERS::charAt)
+                .map(Object::toString)
+                .collect(Collectors.joining());
+    }
+}
