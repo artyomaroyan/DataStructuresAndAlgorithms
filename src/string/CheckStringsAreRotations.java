aac85b5463764e67a887d3319153b0db5f4bb68a
Artyom <aroyanart98@gmail.com>
2025-09-28 16:55:20 +0400

    feat: add `CheckStringsAreRotations` task.

---
+++ src/string/CheckStringsAreRotations.java
@@ -0,0 +1,12 @@
+package string;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 28.09.25
+ * Time: 16:53:05
+ * Task011
+ * Check if two strings are rotations of each other.
+ */
+public class CheckStringsAreRotations {
+
+}
