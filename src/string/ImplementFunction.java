6724728c760b23c2ce0606d725e4d1ce4505f110
Artyom <aroyanart98@gmail.com>
2025-09-28 16:56:41 +0400

    feat: add `ImplementFunction` task.

---
+++ src/string/ImplementFunction.java
@@ -0,0 +1,11 @@
+package string;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 28.09.25
+ * Time: 16:55:53
+ * Task012
+ * Implement strstr() (indexOf) function.
+ */
+public class ImplementFunction {
+}
