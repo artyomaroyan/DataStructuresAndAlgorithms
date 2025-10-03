50480160e2249e6471ae3e10e2021bb6d38c191e
Artyom <aroyanart98@gmail.com>
2025-10-03 22:43:26 +0400

    feat: create task `CountWordsInString`.

---
+++ src/string/CountWordsInString.java
@@ -0,0 +1,11 @@
+package string;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 03.10.25
+ * Time: 22:41:58
+ * Task014
+ * Count the number of words in a string.
+ */
+public class CountWordsInString {
+}
