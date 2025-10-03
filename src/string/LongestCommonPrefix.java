8a365d181387e4b66e55e3835ad0fc6cabd15aad
Artyom <aroyanart98@gmail.com>
2025-10-03 22:42:35 +0400

    feat: create task `LongestCommonPrefix`.

---
+++ src/string/LongestCommonPrefix.java
@@ -0,0 +1,11 @@
+package string;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 03.10.25
+ * Time: 22:41:22
+ * Task013
+ * Longest common prefix among a set of strings.
+ */
+public class LongestCommonPrefix {
+}
