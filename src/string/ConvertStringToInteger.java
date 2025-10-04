938adae8b50d0c68c8ee0fa14e7dc730129a6971
Artyom <aroyanart98@gmail.com>
2025-10-04 22:12:13 +0400

    feat: create task `ConvertStringToInteger`.

---
+++ src/string/ConvertStringToInteger.java
@@ -0,0 +1,11 @@
+package string;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 04.10.25
+ * Time: 22:10:46
+ * Task016
+ * Convert a string to integer (without using Integer.parseInt).
+ */
+public class ConvertStringToInteger {
+}
