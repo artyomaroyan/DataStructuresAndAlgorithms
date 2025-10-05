6a29f551f782e94ba94a01a995852fd614e03bd0
Artyom <aroyanart98@gmail.com>
2025-10-05 22:44:51 +0400

    feat: create task `CapitalizeFirstLetter`.

---
+++ src/string/CapitalizeFirstLetter.java
@@ -0,0 +1,11 @@
+package string;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 05.10.25
+ * Time: 22:44:25
+ * Task017
+ * Capitalize the first letter of each word in a string.
+ */
+public class CapitalizeFirstLetter {
+}
