977bc1436066dd0f488b9dc93f0db2ee8d2b5fe7
Artyom <aroyanart98@gmail.com>
2025-10-06 22:58:22 +0400

    feat: create task `CheckStringSubsequence`.

---
+++ src/string/CheckStringSubsequence.java
@@ -0,0 +1,11 @@
+package string;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 06.10.25
+ * Time: 22:57:44
+ * Task020
+ * Check if a string is a subsequence of another string.
+ */
+public class CheckStringSubsequence {
+}
