d69026629c59df899547a9282438d5a39740fd9d
Artyom <aroyanart98@gmail.com>
2025-10-11 00:06:54 +0400

    feat: create task `RemoveDuplicates`.

---
+++ src/linkedlist/RemoveDuplicates.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+import java.util.logging.Logger;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 11.10.25
+ * Time: 00:05:55
+ * Task009
+ * Remove duplicates from a sorted linked list.
+ */
+public class RemoveDuplicates {
+}
