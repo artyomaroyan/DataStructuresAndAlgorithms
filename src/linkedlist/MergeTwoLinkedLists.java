f62b2c59a9d23985c65576ca6cd5dd2e92238d92
Artyom <aroyanart98@gmail.com>
2025-10-13 23:33:37 +0400

    feat: create `MergeTwoLinkedLists` task.

---
+++ src/linkedlist/MergeTwoLinkedLists.java
@@ -0,0 +1,18 @@
+package linkedlist;
+
+import java.util.logging.Logger;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 13.10.25
+ * Time: 23:31:16
+ * Task012
+ * Merge two sorted linked lists.
+ */
+public class MergeTwoLinkedLists {
+    private static final Logger logger = Logger.getLogger(MergeTwoLinkedLists.class.getName());
+
+    static void main() {
+
+    }
+}
