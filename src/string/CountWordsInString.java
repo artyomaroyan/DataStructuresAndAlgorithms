90018313a1b22e55c68ca053830f34d79780500d
Artyom <aroyanart98@gmail.com>
2025-10-17 22:18:15 +0400

    refactor: implement solution for `CountWordsInString` task.

--- src/string/CountWordsInString.java
+++ src/string/CountWordsInString.java
@@ -8,4 +8,23 @@ package string;
  * Count the number of words in a string.
  */
 public class CountWordsInString {
+    static void main() {
+        test("Test_1", "java, c++, python, c#");
+        test("Test_2", "one, two, three, four");
+        test("Test_3", "count words  in  the  array of string");
+        test("Test_4", "test different   conditions for   logic,   ");
+    }
+
+    private static void test(String label, String str) {
+        var method1 = splitString(str);
+        IO.println(label + " method_1 " + method1);
+    }
+
+    private static int splitString(String string) {
+        if (string == null) return 0;
+        string = string.trim();
+        if (string.isEmpty()) return 0;
+        String[] words = string.split("\\s+");
+        return words.length;
+    }
 }
