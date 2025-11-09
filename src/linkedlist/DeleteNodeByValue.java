097f0a6d806c6874af2a7b1bc38b4a3a496a22e8
Artyom <aroyanart98@gmail.com>
2025-11-09 20:55:05 +0400

    refactor: implement generic linked list solution for `DeleteNodeByValue` class.

--- src/linkedlist/DeleteNodeByValue.java
+++ src/linkedlist/DeleteNodeByValue.java
@@ -9,6 +9,14 @@ package linkedlist;
  */
 public class DeleteNodeByValue {
     static void main() {
+        GenericLinkedList<Integer> linkedList = new GenericLinkedList<>();
+        linkedList.add(45345);
+        linkedList.add(9892);
+        linkedList.add(9092348);
+        linkedList.display();
 
+        linkedList.delete(12312432);
+        linkedList.delete(9892);
+        linkedList.display();
     }
 }
