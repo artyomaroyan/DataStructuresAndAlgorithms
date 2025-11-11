c84749fa93c97e679896fbb8e7bbf127c0663974
Artyom <aroyanart98@gmail.com>
2025-11-11 22:46:54 +0400

    style: cleanup `FindStartingNode` class.

--- src/linkedlist/FindStartingNode.java
+++ src/linkedlist/FindStartingNode.java
@@ -1,10 +1,13 @@
+package linkedlist;
+
 /**
  * Author: Artyom Aroyan
  * Date: 13.10.25
- * Time: 23:11:49
+ * Time: 23:32:40
  * Task011
  * Find the starting node of loop.
  */
-void main() {
-
+public class FindStartingNode {
+    static void main() {
+    }
 }
