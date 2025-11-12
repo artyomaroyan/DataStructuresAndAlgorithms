fc08750c43bc0790a80525665290b850c9b57218
Artyom <aroyanart98@gmail.com>
2025-11-12 22:27:59 +0400

    feat: create `CheckForPalindrome` class.

---
+++ src/linkedlist/CheckForPalindrome.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 12.11.25
+ * Time: 22:27:24
+ * Check if a linked list is palindrome.
+ */
+public class CheckForPalindrome {
+    static void main() {
+
+    }
+}
