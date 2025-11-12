ca9662556c64775c6e239eee0d8dd0967b47b0a3
Artyom <aroyanart98@gmail.com>
2025-11-12 22:26:53 +0400

    feat: create `AddNumbers` class.

---
+++ src/linkedlist/AddNumbers.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+/**
+ * Author: Artyom Aroyan
+ * Date: 12.11.25
+ * Time: 22:26:14
+ * Add two numbers represented by linked lists.
+ */
+public class AddNumbers {
+    static void main() {
+
+    }
+}
