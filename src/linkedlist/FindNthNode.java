b9cdae86354922cf1050a486d85225c36dc490cf
Artyom <aroyanart98@gmail.com>
2026-01-04 23:43:58 +0400

    refactor: implement `FindNthNode` solution.

--- src/linkedlist/FindNthNode.java
+++ src/linkedlist/FindNthNode.java
@@ -5,9 +5,39 @@ package linkedlist;
  * Date: 10.10.25
  * Time: 08:14:40
  * Task008
- * Find nth node from end.
+ * Find n-th node from end.
  */
 public class FindNthNode {
     static void main() {
+
+    }
+
+    // Two Pointer Approach
+    private static Node<?> findNthFromEnd(Node<?> head, int n) {
+        if (head == null || n <= 0) return null;
+
+        Node<?> first = head;
+        Node<?> second = head;
+
+        for (int i = 0; i < n; i++) {
+            if (first == null) return null;
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        return second;
+    }
+
+    private static final class Node<E> {
+        E element;
+        Node<E> next;
+
+        Node(E element, Node<E> next) {
+            this.element = element;
+            this.next = next;
+        }
     }
 }
