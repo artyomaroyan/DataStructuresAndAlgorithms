f54959d1549fd613a5e1b43feae561f060eec4c5
Artyom <aroyanart98@gmail.com>
2026-01-08 23:12:58 +0400

    refactor: implement solution for `RemoveDuplicates` class.

--- src/linkedlist/RemoveDuplicates.java
+++ src/linkedlist/RemoveDuplicates.java
@@ -9,21 +9,32 @@ package linkedlist;
  */
 public class RemoveDuplicates {
     static void main() {
-
     }
 
     // Iterative Approach
     private static Node<?> removeDuplicatesIterative(Node<?> head) {
-        return null;
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node<?> current = head;
+        while (current != null && current.next != null) {
+            if (current.element == current.next.element) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+        return current;
     }
 
     private static final class Node<E> {
         E element;
-        Node<E> next;
+        Node<?> next;
 
-        Node(E element, Node<E> next) {
+        Node(E element) {
             this.element = element;
-            this.next = next;
+            this.next = null;
         }
     }
 }
