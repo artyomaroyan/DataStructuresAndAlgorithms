bab2c421ff63c24ccbf59abd701a004858911837
Artyom <aroyanart98@gmail.com>
2026-04-09 15:37:20 +0400

    refactor: test

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,13 @@
  * Time: 23:46:27
  */
 void main() {
-//    LinkedList<String> linkedList = new LinkedList<>();
+    int num = 12154;
+    String number = Integer.toString(num);
+    String reversed;
+    char[] character = number.toCharArray();
+
+    for (int i = character.length - 1; i >= 0; i--) {
+        reversed = String.valueOf(character[i]);
+        IO.println(reversed);
+    }
 }
