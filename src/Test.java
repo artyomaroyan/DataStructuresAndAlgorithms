d278b20b0edfaf8804a6caab96e7e0a577dddfe5
Artyom <aroyanart98@gmail.com>
2026-04-10 15:44:14 +0400

    refactor: `CheckForPalindrome` solved:

--- src/Test.java
+++ src/Test.java
@@ -6,36 +6,12 @@ import static java.lang.IO.*;
  * Time: 23:46:27
  */
 void main() {
-    LinkedList<Integer> list = new LinkedList<>();
-    list.add(428);
-    list.add(54);
-    list.add(251);
-    list.add(3717);
-    list.add(78);
-    list.add(1510);
-    list.add(43);
-//    list.add(9845);
-//    list.add(61);
-//    list.add(9131);
-//    list.add(62);
-//    list.add(25);
-//    list.add(6379);
-//    list.add(50);
-//    list.add(71);
-//    list.add(5803);
-//    list.add(24);
-//    list.add(6154);
-//    list.add(79);
-//    list.add(9131);
-//    list.add(30);
-//    list.add(9230);
-//    list.add(66);
-//    list.add(8042);
-//    list.add(49);
+    var list = createList(10, 43, 55, 55, 43, 10, 9);
+    var reversed = list.reversed();
+    println("List1 - " + list);
+    println("List1 - " + reversed);
+}
 
-    Integer pivot1 = list.get(list.size() >> 1);
-    println("result of Integer pivot = list.get(list.size() >> 2); - " + pivot1);
-
-    Integer pivot2 = list.size() >> 1;
-    println("result of Integer pivot2 = list.size() >> 2; - " + pivot2);
+private static LinkedList<Integer> createList(Integer... values) {
+    return new LinkedList<>(List.of(values));
 }
--- src/linkedlist/CheckForPalindrome.java
+++ src/linkedlist/CheckForPalindrome.java
@@ -1,13 +1,40 @@
 package linkedlist;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.IO.println;
+
 /**
  * Author: Artyom Aroyan
  * Date: 12.11.25
  * Time: 22:27:24
+ * Task014
  * Check if a linked list is palindrome.
  */
 public class CheckForPalindrome {
     static void main() {
+        var list = createList(1, 2, 3, 1);
+        var reversed = list.reversed();
+        println("List1 - " + list);
+        println("List1 - " + reversed);
 
+        println(isPalindrome(list, reversed));
+    }
+
+    private static boolean isPalindrome(LinkedList<Integer> original, LinkedList<Integer> reversed) {
+        if (original.size() != reversed.size()) return false;
+
+        for (int i = 0; i < original.size(); i++) {
+            if (!original.get(i).equals(reversed.get(i))) {
+                return false;
+            }
+        }
+        return true;
+//        return original.equals(reversed); // better approach
+    }
+
+    private static LinkedList<Integer> createList(Integer... values) {
+        return new LinkedList<>(List.of(values));
     }
 }
