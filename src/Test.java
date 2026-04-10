2f8d350413216cad30c14ce4dc63f83588bf0b11
Artyom <aroyanart98@gmail.com>
2026-04-10 15:00:39 +0400

    refactor: `MergeTwoLinkedLists` solved.

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,41 @@
+import static java.lang.IO.*;
+
 /**
  * Author: Artyom Aroyan
  * Date: 13.10.25
  * Time: 23:46:27
  */
 void main() {
-    int num = 12154;
-    String number = Integer.toString(num);
-    String reversed;
-    char[] character = number.toCharArray();
+    LinkedList<Integer> list = new LinkedList<>();
+    list.add(428);
+    list.add(54);
+    list.add(251);
+    list.add(3717);
+    list.add(78);
+    list.add(1510);
+    list.add(43);
+//    list.add(9845);
+//    list.add(61);
+//    list.add(9131);
+//    list.add(62);
+//    list.add(25);
+//    list.add(6379);
+//    list.add(50);
+//    list.add(71);
+//    list.add(5803);
+//    list.add(24);
+//    list.add(6154);
+//    list.add(79);
+//    list.add(9131);
+//    list.add(30);
+//    list.add(9230);
+//    list.add(66);
+//    list.add(8042);
+//    list.add(49);
 
-    for (int i = character.length - 1; i >= 0; i--) {
-        reversed = String.valueOf(character[i]);
-        IO.println(reversed);
-    }
+    Integer pivot1 = list.get(list.size() >> 1);
+    println("result of Integer pivot = list.get(list.size() >> 2); - " + pivot1);
+
+    Integer pivot2 = list.size() >> 1;
+    println("result of Integer pivot2 = list.size() >> 2; - " + pivot2);
 }
--- src/linkedlist/MergeTwoLinkedLists.java
+++ src/linkedlist/MergeTwoLinkedLists.java
@@ -1,5 +1,10 @@
 package linkedlist;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import static java.lang.IO.println;
+
 /**
  * Author: Artyom Aroyan
  * Date: 13.10.25
@@ -10,5 +15,63 @@ package linkedlist;
 public class MergeTwoLinkedLists {
     static void main() {
 
+        // 1. create 2 linked list.
+        LinkedList<Integer> list1 = createList(4807, 6757, 8907, 8334,6550, 7879, 7879);
+        println("list 1 before sort - " + list1);
+
+        LinkedList<Integer> list2 = createList(1991, 6847, 64, 3801, 3697, 6240, 735, 9412, 5515);
+        println("list 2 before sort - " + list2);
+
+        // 2. sort them.
+        var sortedList1 = quickSort(list1);
+        var sortedList2 = quickSort(list2);
+
+        // 3. marge two sorted linked lists.
+        var merged = mergeSortedLists(sortedList1, sortedList2);
+        println("Merged list " + merged);
+    }
+
+    private static LinkedList<Integer> mergeSortedLists(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+        LinkedList<Integer> result = new LinkedList<>();
+        int i = 0, j = 0;
+
+        while (i < list1.size() && j < list2.size()) {
+            if (list1.get(i) <= list2.get(j)) {
+                result.add(list1.get(i++));
+            } else {
+                result.add(list2.get(j++));
+            }
+        }
+
+        while (i < list1.size()) result.add(list1.get(i++));
+        while (j < list2.size()) result.add(list2.get(j++));
+
+        return result;
+    }
+
+    private static LinkedList<Integer> quickSort(LinkedList<Integer> list) {
+        if (list.size() < 2) return list;
+
+        LinkedList<Integer> smaller = new LinkedList<>();
+        LinkedList<Integer> same = new LinkedList<>();
+        LinkedList<Integer> larger = new LinkedList<>();
+
+        Integer pivot = list.get(list.size() >> 1);
+
+        for (Integer element : list) {
+            if (element < pivot) smaller.add(element);
+            else if (element > pivot) larger.add(element);
+            else same.add(element);
+        }
+
+        LinkedList<Integer> result = new LinkedList<>();
+        result.addAll(quickSort(smaller));
+        result.addAll(same);
+        result.addAll(quickSort(larger));
+        return result;
+    }
+
+    private static LinkedList<Integer> createList(Integer... values) {
+        return new LinkedList<>(List.of(values));
     }
 }
