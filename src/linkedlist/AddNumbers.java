1b4adb13c4ef97fd74c4768a8783095f95428ab5
Artyom <aroyanart98@gmail.com>
2026-04-10 15:15:06 +0400

    refactor: todo:

--- src/linkedlist/AddNumbers.java
+++ src/linkedlist/AddNumbers.java
@@ -4,10 +4,11 @@ package linkedlist;
  * Author: Artyom Aroyan
  * Date: 12.11.25
  * Time: 22:26:14
+ * Task013
  * Add two numbers represented by linked lists.
  */
 public class AddNumbers {
     static void main() {
-
+        // todo
     }
 }
--- src/linkedlist/RemoveLoop.java
+++ src/linkedlist/RemoveLoop.java
@@ -4,9 +4,11 @@ package linkedlist;
  * Author: Artyom Aroyan
  * Date: 12.11.25
  * Time: 22:28:43
+ * Task015
+ * Remove loop from linked list.
  */
 public class RemoveLoop {
     static void main() {
-
+        // todo
     }
 }
